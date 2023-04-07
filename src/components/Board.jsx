84ce08ffc79f9f9b88212114b04c427068ae77e9
Jorge <aportuvas@outlook.es>
2023-04-07 20:11:08 +0200

    fix: Refactor code

---
+++ src/components/Board.jsx
@@ -0,0 +1,20 @@
+import Square from './Square.jsx'
+
+function Board({ board, updateBoard }) {
+  return (
+    <main className='board'>
+      {board.map((cell, index) => {
+        return (
+          <Square
+            key={index}
+            index={index}
+            updateBoard={updateBoard}>
+            {cell}
+          </Square>
+        )
+      })}
+    </main>
+  )
+}
+
+export default Board
---
+++ src/logic/board.js
@@ -0,0 +1,20 @@
+import { WINNER_COMBOS } from '../constans'
+
+export const checkWinner = (boardToCheck) => {
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  //Si no hay ganador
+  return null
+}
+
+export const checkEndGame = (newBoard) => {
+  return newBoard.every((square) => square !== null)
+}
